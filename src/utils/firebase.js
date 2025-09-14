e6a207f2f33d98ffdfbdb7051a4b54f3b6efbb6f
Anas Khan <annaasskhan6@gmail.com>
2025-09-14 14:38:58 +0530

    Setup Firebase Hosting with GitHub Actions

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBd9CcxJac2qpqY20GYTiFE0DVw2J_H1uE",
+  authDomain: "watchparty-3d876.firebaseapp.com",
+  projectId: "watchparty-3d876",
+  storageBucket: "watchparty-3d876.firebasestorage.app",
+  messagingSenderId: "789876258013",
+  appId: "1:789876258013:web:147c08ee0e68e5aa7a1734",
+  measurementId: "G-H6PNKCKZYC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
